15a4d07e5b35ba2b10586a7ce18de861d7db7708
Vinaud <leyenasd@gmail.com>
2026-02-11 15:46:55 -0300

    Cenário de validação de pesquisa sem resultado

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -20,3 +20,26 @@ test('Deve consultar um pedido aprovado', async ({ page }) => {
   await expect(page.locator('text=APROVADO')).toBeVisible();
 
 });
+
+test('Deve exibir mensagem de erro para um pedido não encontrado', async ({ page }) => {
+
+  const pedido = 'VLO-ABC123';
+  await page.goto('http://localhost:5173/');
+
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(pedido); 
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+
+
+  const titulo = page.getByRole( 'heading', {name: 'Pedido não encontrado'})
+  await expect(titulo).toBeVisible()
+
+  const mensagem = page.locator('p', {hasText: 'Verifique o número do pedido e tente novamente'})
+  await expect(mensagem).toBeVisible()
+
+});
+
