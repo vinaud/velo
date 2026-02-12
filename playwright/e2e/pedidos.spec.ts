a50517eedf31784ce2a3bb521d74a494b0528764
Vinaud <leyenasd@gmail.com>
2026-02-12 19:50:56 -0300

    Implementação inicial de hooks

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,52 +1,73 @@
 import { test, expect } from '@playwright/test';
 import { gerarCodigoPedido } from '../support/helpers';
 
-test('Deve consultar um pedido aprovado', async ({ page }) => {
+test.describe('Consulta de pedido', () =>{
 
-  const pedido = 'VLO-H8LZEN';
-  await page.goto('http://localhost:5173/');
+  test.beforeAll(async () => {
+    console.log(
+      'beforeAll: roda uma vez antes de todos os testes.'
+    )
+  })
+  
+  test.beforeEach(async ({page}) => {
+    await page.goto('http://localhost:5173/');
+  
+    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+  
+    await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+    await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+  })
+  
+  test.afterEach(async () => {
+    console.log(
+      'afterEach: roda depois de cada teste.'
+    )
+  })
+  
+  test.afterAll(async () => {
+    console.log(
+      'afterAll: roda uma vez depois de todos os testes.'
+    )
+  })
+  
 
-  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+  test('Deve consultar um pedido aprovado', async ({ page }) => {
 
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+    const pedido = 'VLO-H8LZEN';
+  
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(pedido); 
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+  
+   
+    const containerPedido = page.getByRole('paragraph').filter({ hasText: /^Pedido$/ }).locator('..');
+    await expect(containerPedido).toContainText(pedido);
+   
+    await expect(page.locator('text=APROVADO')).toBeVisible();
+  
+  });
+  
+  test('Deve exibir mensagem de erro para um pedido não encontrado', async ({ page }) => {
+  
+    const pedido = gerarCodigoPedido()
+  
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(pedido); 
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+  
+  
+    const titulo = page.getByRole( 'heading', {name: 'Pedido não encontrado'})
+    await expect(titulo).toBeVisible()
+  
+    const mensagem = page.locator('p', {hasText: 'Verifique o número do pedido e tente novamente'})
+    await expect(mensagem).toBeVisible()
+  
+    await expect(page.locator('#root')).toMatchAriaSnapshot(`
+      - img
+      - heading "Pedido não encontrado" [level=3]
+      - paragraph: Verifique o número do pedido e tente novamente
+      `)
+  
+  });
+  
+})
 
-  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(pedido); 
-  await page.getByRole('button', { name: 'Buscar Pedido' }).click();
-
- 
-  const containerPedido = page.getByRole('paragraph').filter({ hasText: /^Pedido$/ }).locator('..');
-  await expect(containerPedido).toContainText(pedido);
- 
-  await expect(page.locator('text=APROVADO')).toBeVisible();
-
-});
-
-test('Deve exibir mensagem de erro para um pedido não encontrado', async ({ page }) => {
-
-  const pedido = gerarCodigoPedido()
-  await page.goto('http://localhost:5173/');
-
-  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
-
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
-
-  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(pedido); 
-  await page.getByRole('button', { name: 'Buscar Pedido' }).click();
-
-
-  const titulo = page.getByRole( 'heading', {name: 'Pedido não encontrado'})
-  await expect(titulo).toBeVisible()
-
-  const mensagem = page.locator('p', {hasText: 'Verifique o número do pedido e tente novamente'})
-  await expect(mensagem).toBeVisible()
-
-  await expect(page.locator('#root')).toMatchAriaSnapshot(`
-    - img
-    - heading "Pedido não encontrado" [level=3]
-    - paragraph: Verifique o número do pedido e tente novamente
-    `)
-
-});
 
