4cd4992c40a73bf54ea1741f2f12454997f03475
Vinaud <leyenasd@gmail.com>
2026-02-14 09:45:11 -0300

    Validação de novos cenários de consulta

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -58,8 +58,9 @@ test.describe('Consulta de pedido', () =>{
     - img
     - paragraph: Pedido
     - paragraph: ${pedido.numero}
-    - img
-    - text: ${pedido.status}
+    - status:
+        - img
+        - text: ${pedido.status}
     - img "Velô Sprint"
     - paragraph: Modelo
     - paragraph: Velô Sprint
@@ -82,6 +83,13 @@ test.describe('Consulta de pedido', () =>{
     - paragraph: ${pedido.pagamento}
     - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
     `);
+
+    const statusBadge = page.getByRole('status').filter({hasText: pedido.status})
+    await expect(statusBadge).toHaveClass(/bg-green-100/)
+    await expect(statusBadge).toHaveClass(/text-green-700/)
+
+    const statusIcon = statusBadge.locator( 'svg')
+    await expect(statusIcon).toHaveClass(/lucide-circle-check-big/)
 })
 
   test('Deve consultar um pedido reprovado', async ({ page }) => {
@@ -105,8 +113,9 @@ test.describe('Consulta de pedido', () =>{
       - img
       - paragraph: Pedido
       - paragraph: ${pedido.numero}
-      - img
-      - text: ${pedido.status}
+      - status:
+        - img
+        - text: ${pedido.status}
       - img "Velô Sprint"
       - paragraph: Modelo
       - paragraph: Velô Sprint
@@ -129,6 +138,69 @@ test.describe('Consulta de pedido', () =>{
       - paragraph: ${pedido.pagamento}
       - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
       `);
+
+      const statusBadge = page.getByRole('status').filter({hasText: pedido.status})
+      await expect(statusBadge).toHaveClass(/bg-red-100/)
+      await expect(statusBadge).toHaveClass(/text-red-700/)
+  
+      const statusIcon = statusBadge.locator( 'svg')
+      await expect(statusIcon).toHaveClass(/lucide-circle-x/)
+
+  })
+
+  test('Deve consultar um pedido em análise', async ({ page }) => {
+    
+    const pedido = {
+      numero: 'VLO-L893TT',
+      status: 'EM_ANALISE',
+      cor: 'Glacier Blue',
+      roda: 'aero Wheels',
+      cliente: {
+        nome:  'Adalberto Silva',
+        email: 'email@email.com'
+      },
+      pagamento: 'À Vista'
+    }
+  
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(pedido.numero); 
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+
+    await expect(page.getByTestId(`order-result-${pedido.numero}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${pedido.numero}
+      - status:
+        - img
+        - text: ${pedido.status}
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: ${pedido.cor}
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: ${pedido.roda}
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: ${pedido.cliente.nome}
+      - paragraph: Email
+      - paragraph: ${pedido.cliente.email}
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: ${pedido.pagamento}
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+      `);
+
+      const statusBadge = page.getByRole('status').filter({hasText: pedido.status})
+      await expect(statusBadge).toHaveClass(/bg-amber-100/)
+      await expect(statusBadge).toHaveClass(/text-amber-700/)
+  
+      const statusIcon = statusBadge.locator( 'svg')
+      await expect(statusIcon).toHaveClass(/lucide-clock/)
   })
   
   test('Deve exibir mensagem de erro para um pedido não encontrado', async ({ page }) => {
