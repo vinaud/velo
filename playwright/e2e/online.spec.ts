01414ca45218481e43b7d390f342a8a7a766f803
Vinaud <leyenasd@gmail.com>
2026-05-06 06:31:36 -0300

    feat: configure Vercel deployment and add E2E connectivity test for production URL

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '../support/fixtures'
 
 test('O aplicativo deve estar online', async ({ page }) => {
-  await page.goto('http://localhost:5173')
+  await page.goto('/')
   await expect(page).toHaveTitle(/Velô by Papito/)
 })
 
